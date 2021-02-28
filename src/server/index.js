672d303a5e49bf02457eeac92d75f83d54174a43
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2021-02-28 12:39:53 +0100

    add package.json to the server

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/api/test", (req, res) => {
   res.send({
     test: "test!",
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("listening on port 3000");
 });
