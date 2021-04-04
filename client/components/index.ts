63dabc5690a5c37a4789fe37a636be9c99fa22ef
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2021-04-04 22:07:44 +0200

    fix typo

--- client/components/index.ts
+++ client/components/index.ts
@@ -1,6 +1,6 @@
 import NotStarted from "./notStarted";
 import Ended from "./ended";
 import PitchVisualization from "./pitchVisualization";
-import GameHeader from "./GameHeader";
+import GameHeader from "./gameHeader";
 
 export { NotStarted, Ended, PitchVisualization, GameHeader };
