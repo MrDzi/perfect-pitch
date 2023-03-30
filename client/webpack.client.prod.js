603b0e4ab52100a4b0815bac55c265cbde48d4b6
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2023-03-30 18:33:54 +0200

    fix bundling images for prod

--- client/webpack.client.prod.js
+++ client/webpack.client.prod.js
@@ -54,12 +54,12 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
       },
     ],
   },
