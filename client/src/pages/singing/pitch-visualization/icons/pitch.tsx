30b612faa5ee59616d9377ee7f88a224064f74fd
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2024-09-04 10:53:14 +0200

    add small styling changes

--- client/src/pages/singing/pitch-visualization/icons/pitch.tsx
+++ client/src/pages/singing/pitch-visualization/icons/pitch.tsx
@@ -4,7 +4,7 @@ const Pitch = (): ReactElement => (
   <svg width="134" height="84">
     <g>
       <path
-        fill="#E56B6F"
+        fill="#333533"
         d="M67,0 C93,0 96,42 134,42 C96,42 93,84 67,84 C41,84 36,42 0,42 C36,42 41,1.9959915e-15 67,0 Z"
       />
     </g>
