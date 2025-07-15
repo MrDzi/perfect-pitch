a6dd234279b00f233cccd246b3c6e6389c1f1895
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2025-07-15 23:12:44 +0200

    add/update text on home page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -9,7 +9,7 @@ const Home = (): ReactElement => {
   const [navigate] = useNavigateWithTransition();
 
   useEffect(() => {
-    document.title = "Home | CheckYourPitch";
+    document.title = "Pitch Training Games | CheckYourPitch - Free Ear Training";
   }, []);
 
   const onLinkClick = (route: string) => {
@@ -17,6 +17,14 @@ const Home = (): ReactElement => {
   };
   return (
     <div className="page page--home">
+      <div className="home-header">
+        <h1 className="home-header_title">Check Your Pitch</h1>
+        <h2 className="home-header_subtitle">🎶 Train your ear. Play and improve. 🎶</h2>
+        <p className="home-header_desc">
+          No downloads or sign‑ups. Just simple and fun effective ear‑training games you can enjoy instantly in your
+          browser.
+        </p>
+      </div>
       <div className="home-content-wrapper">
         <div className="home-content">
           <div className="home-content_block home-content_block--big" onClick={onLinkClick("/pitchle")}>
@@ -24,7 +32,10 @@ const Home = (): ReactElement => {
             <div className="home-content_block_inner" />
             <div className="home-content_desc">
               <h3>Pitchle</h3>
-              <p>A musical twist on Wordle. Guess each tone in today&apos;s melody!</p>
+              <p>
+                A musical twist on Wordle. Listen carefully and guess each note in today’s hidden melody. Can you get it
+                in six tries?
+              </p>
             </div>
             <span className="home-content_cta button button--small">Play Now</span>
           </div>
@@ -33,7 +44,7 @@ const Home = (): ReactElement => {
             <div className="home-content_block_inner" />
             <div className="home-content_desc">
               <h3>Can you sing this tone?</h3>
-              <p>Test your pitch accuracy by repeating five random tones.</p>
+              <p>Test your pitch accuracy by singing back five random tones. See how close you can match them!</p>
             </div>
             <span className="home-content_cta button button--small">Play Now</span>
           </div>
@@ -42,16 +53,13 @@ const Home = (): ReactElement => {
             <div className="home-content_block_inner" />
             <div className="home-content_desc">
               <h3>How good is your ear?</h3>
-              <p>Challenge your ears - identify small pitch differences.</p>
+              <p>
+                Challenge yourself to spot subtle pitch differences. Which tone is higher, lower, or exactly the same?
+              </p>
             </div>
             <span className="home-content_cta button button--small">Play Now</span>
           </div>
         </div>
-      </div>
-      <div className="coffee-button-wrapper">
-        <a href="https://www.buymeacoffee.com/steam_roller" target="_blank" rel="noreferrer">
-          <img src="https://cdn.buymeacoffee.com/buttons/v2/default-black.png" alt="Buy Me A Coffee" width="110" />
-        </a>
       </div>
     </div>
   );
